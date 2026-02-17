b8b12b109005a4595aea8e357c5bc9164a753e2d
Nathan Wanjongkhum <wanjongkhumnathan@gmail.com>
2026-02-17 13:09:30 -0800

    Prints pruning table

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use crate::pruning_table::PruningTables;
+
 mod cubie_cube;
 mod pruning_table;
 mod turn;
 
 fn main() {
-    println!("asd");
+    let tables = PruningTables::new();
+    println!("{:#?}", tables.twist_move.len());
+    println!("{:#?}", tables.flip_move.len());
+    println!("{:#?}", tables.slice_move.len());
 }
